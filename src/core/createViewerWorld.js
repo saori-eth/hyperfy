1328f4124daa3730f797ff68f1ea5aaf6a258d7f
Ash <hello@ashconnell.com>
2025-03-04 23:45:16 +1100

    imp: add controls systems to viewer world

--- src/core/createViewerWorld.js
+++ src/core/createViewerWorld.js
@@ -3,6 +3,7 @@ import { World } from './World'
 import { Client } from './systems/Client'
 import { ClientPrefs } from './systems/ClientPrefs'
 import { ClientLoader } from './systems/ClientLoader'
+import { ClientControls } from './systems/ClientControls'
 import { ClientGraphics } from './systems/ClientGraphics'
 import { ClientEnvironment } from './systems/ClientEnvironment'
 // import { ClientAudio } from './systems/ClientAudio'
@@ -14,6 +15,7 @@ export function createViewerWorld() {
   world.register('client', Client)
   world.register('prefs', ClientPrefs)
   world.register('loader', ClientLoader)
+  world.register('controls', ClientControls)
   world.register('graphics', ClientGraphics)
   world.register('environment', ClientEnvironment)
   // world.register('audio', ClientAudio)

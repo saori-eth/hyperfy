f0646a3e1cd62c4ac16876d44c4196a525e6f0dc
Ash <hello@ashconnell.com>
2025-03-28 12:42:33 +1100

    fix: webp extension on server preventing app mount
    GLTFLoader was detecting no webp support on the server, which in turn prevented the model from loading, causing the app to never build or run its scripts.

--- src/core/systems/ServerLoader.js
+++ src/core/systems/ServerLoader.js
@@ -1,7 +1,8 @@
 import fs from 'fs-extra'
 import path from 'path'
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { GLTFLoader } from '../libs/gltfloader/GLTFLoader.js'
 // import { VRMLoaderPlugin } from '@pixiv/three-vrm'
 
 import { System } from './System'
